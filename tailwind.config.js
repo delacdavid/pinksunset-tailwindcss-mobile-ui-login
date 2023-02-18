27dc8ae7c09c8fa7fbb02784026f4a95ff7fd30c
David HansaWorld <david@MacBook-Pro-3.local>
2023-02-18 21:23:39 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#ff406f",
+    },
+  },
+  plugins: [],
+};
